a51a66945e7331514c594f26df46bbbb093e91e9
Jason Domingo <jayprdom@udel.edu>
2025-02-24 00:04:25 -0500

    finished tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -20,7 +20,10 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled: number[] = numbers.map(
+        (numbers: number): number => numbers * 3,
+    );
+    return tripled;
 }
 
 /**
